76188979775d96528e815ad69018ad41f2c3c22b
Djabri Maroua <125154966+djabriMaroua@users.noreply.github.com>
2023-10-05 11:15:10 +0100

    Update without echo

--- lib/searchPatient.dart
+++ lib/searchPatient.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'editPatieny.dart';
+
 
 class SearchPatientPage extends StatefulWidget {
   @override
